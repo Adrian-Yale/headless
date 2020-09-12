8e1e559dea41cdde72b7e9232f23c9dadaa2da98
Adrian Yale <hello@adrian-yale.co.uk>
2020-09-12 21:33:23 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Posts from './components/posts.js';
+function App() {
+  return (
+	<div className="App">
+		<Posts />
+	</div>
+  );
+}
+
+export default App;
---
+++ components/posts.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export class Posts extends Component {
+	state = {
+		posts: [],
+		isLoaded: false
+	}
+
+	componentDidMount () {
+
+		
+		axios.get('http://staging.adrian-yale.co.uk/b_e/wp-json/wp/v2/posts')
+			.then(res => this.setState({
+				posts: res.data,
+				isLoaded:true
+			}))
+			.catch( err => console.log(err))
+	}
+
+	render() {
+		console.log(this.state);
+		const {posts} = this.state;
+		return ( 
+			<div className="post-archive">
+				{posts.map(post => 
+					<div className="post-card">
+						<h2>{post.title.rendered}</h2>
+						<div dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+					</div>
+
+				)}
+
+
+			</div>
+		)
+	}
+
+}
+export default Posts
